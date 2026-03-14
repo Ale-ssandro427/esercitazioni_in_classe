892dd209060c2c2a87adc863cefe33f907a5f214
Ale-ssandro427 <alessandro.merola@stu.istitutoturoldo.edu.it>
2026-03-14 12:55:38 +0100

    nuovo file

---
+++ Main/es4.java
@@ -0,0 +1,11 @@
+package Main;
+public class es4 {
+    public static void main(String[] args) {
+        int risultato = somma(5, 7);
+        System.out.println("La somma è: " + risultato);
+    }
+
+    public static int somma(int a, int b) {
+        return a + b;
+    }
+}

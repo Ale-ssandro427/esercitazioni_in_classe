77805e4f264b7079b2e4b54566b3bdfdafdc9610
Ale-ssandro427 <alessandro.merola@stu.istitutoturoldo.edu.it>
2026-03-14 12:43:56 +0100

    nuovo file

---
+++ Main/esercizio2.java
@@ -0,0 +1,12 @@
+package Main;
+public class esercizio2 {
+    public static void main(String[] args) {
+        int numero = 10;
+        double decimale = 3.14;
+        String testo = "Ciao Java";
+
+        System.out.println("Int: " + numero);
+        System.out.println("Double: " + decimale);
+        System.out.println("String: " + testo);
+    }
+}
